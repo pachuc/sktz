39232e1aa4dd8d6b448a550a6a1c0dfd7074136e
pachuc <bpgoku2002@gmail.com>
2016-12-01 09:13:23 -0800

    clean up code for sktz

--- sktz.py
+++ sktz.py
@@ -14,23 +14,7 @@ def controller(ws, controller_id):
     and write the messages of the websocket to redis.
     """
     logging.error('Controller {0} connected.'.format(controller_id))
-
-@sockets.route('/echo')
-def echo_socket(ws):
-    logging.error('Web socket intialized')
+    logging.error('Web socket initialized')
     while not ws.closed:
         message = ws.receive()
-        ws.send(message)
-
-
-@app.route('/')
-def hello():
-    logging.error('This is a test!')
-    return 'Hello World!'
-
-
-if __name__ == "__main__":
-    from gevent import pywsgi
-    from geventwebsocket.handler import WebSocketHandler
-    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
-    server.serve_forever()
+        logging.error('Controller {0} posted message {1}'.format(controller_id, message))
