a99c44c571c7b29cbe8607c29198eacb09250a31
pachuc <bpgoku2002@gmail.com>
2016-12-05 16:08:24 -0800

    some clean ups

--- sktz/static/javascripts/Main.js
+++ sktz/static/javascripts/Main.js
@@ -4,17 +4,19 @@ function Main(game_id, num_controllers){
 	var elem = document.getElementById('Main');
 	var FRAMERATE = 60/1000;//60 fps
 
-    var two = new Two({
-    	width: w,
-    	height: h
-    });
+    	var two = new Two({
+    		width: w,
+    		height: h
+    	});
     	var game_state_string = 'http://localhost:8000/get_game_state/' + game_id;
 
-
-    two.appendTo(elem);
-    var game = new Game(two, w, h, num_controllers);
+    
+    	two.appendTo(elem);
+    	var game = new Game(two, w, h, num_controllers);
 
 	two.bind('update', function(frameCount){
+		//yeah apparently i shouldn't be making synchronous ajax calls as I'm basically blocking the UI thread with a get
+		//and could possibly cause the browser to hang/crash
 		var controller_data = JSON.parse($.ajax({type: "GET", url: game_state_string, async:false}).responseText);
                 game.update(controller_data);
 	});
