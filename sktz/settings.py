236b568a6ab401e6580b0e91eddc71714535a250
pachuc <bpgoku2002@gmail.com>
2016-12-07 14:13:38 -0800

    add logging dict to settings

--- sktz/settings.py
+++ sktz/settings.py
@@ -2,3 +2,36 @@ REDIS_HOST = 'redis'
 REDIS_PORT = 6379
 REDIS_DB = 0
 REDIS_URL = 'redis://{host}:{port}/{db}'.format(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+           'format':'%(asctime)s %(process)d %(processName)s %(levelname)s %(name)s:%(lineno)d %(message)s' 
+        },
+        'simple': {
+           'format':'%(asctime)s %(levelname)5s  %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'syslog': {
+            'level': 'INFO',
+            'class': 'logging.handlers.SysLogHandler',
+            'formatter': 'verbose',
+            'address': ('localhost', 514),
+        }
+    },
+    'loggers': {
+        'sktz': {
+            'handlers': ['console', 'syslog'],
+            'level': 'DEBUG',
+            'propagate': False
+        }
+    }
+}
