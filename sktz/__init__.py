d483111f129720de24c0dcdb85b9c5c7739308b4
pachuc <bpgoku2002@gmail.com>
2016-12-06 16:15:23 -0800

    cleaned up by adding helper functions and wrapper to avoid code duplication

--- sktz/__init__.py
+++ sktz/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sockets import Sockets
+from functools import wraps
 import logging
 import redis
 import uuid
@@ -10,48 +11,82 @@ app = Flask(__name__)
 sockets = Sockets(app)
 r = redis.StrictRedis(host='redis', port=6379, db=0)
 
+def _getGame(game_id):
+    return json.loads(r.get(game_id))
+def _getController(game_id, controller_id):
+    redis_key = '{0}/{1}'.format(game_id, controller_id)
+    return json.loads(r.get(redis_key))
+def _setGame(game_id, game_state):
+    r.set(game_id, json.dumps(game_state))
+def _setController(game_id, controller_id, controller_state):
+    redis_key = '{0}/{1}'.format(game_id, controller_id)
+    r.set(redis_key, json.dumps(controller_state))
+def game_exists(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        game_id = kwargs['game_id']
+        if r.exists(game_id):
+            return f(*args, **kwargs)
+        else:
+            logging.error('Game {0} does not exist'.format(game_id))
+    return decorated_function
+
 @app.route('/start_game/<num_controllers>')
 def start_game(num_controllers):
     game_id = uuid.uuid4()
+    
     game_state = {}
     game_state['status'] = 'INIT'
     game_state['num_controllers'] = int(num_controllers)
     controller_state = {}
     controller_state['status'] = 'DISCONNECTED'
     controller_state['input'] = {}
-    for i in xrange(0, int(num_controllers)):
-        redis_key = '{0}/{1}'.format(game_id, i)
-        r.set(redis_key, json.dumps(controller_state))
-    r.set(game_id, json.dumps(game_state))
+    
+    for controller_id in xrange(0, int(num_controllers)):
+        _setController(game_id, controller_id, controller_state)
+    _setGame(game_id, game_state)
+    
     logging.error('Initalized new game {0} ({1})'.format(game_id, game_state))
     return render_template('Game.html', game_id=str(game_id), num_controllers=num_controllers)
 
+
 @sockets.route('/connect_controller/<game_id>/<controller_id>')
+@game_exists
 def connect_controller(ws, game_id, controller_id):
-    if not r.exists(game_id):
-        logging.error('Game {0} does not exist, could not connect.'.format(game_id))
-        return 'Game {0} does not exist'.format(game_id)
-   
     logging.error('Controller {0} connected to game {1}.'.format(controller_id, game_id))
-    redis_key = '{0}/{1}'.format(game_id, controller_id)
-    controller_state = json.loads(r.get(redis_key))
+    controller_state = _getController(game_id, controller_id)
     controller_state['status'] = 'CONNECTED'
     while not ws.closed:
         message = ws.receive()
         if message is not None:
             controller_state['input'] = message
-        r.set(redis_key, json.dumps(controller_state))
+        _setController(game_id, controller_id, controller_state)
     controller_state['status'] = 'DISCONNECTED'
-    r.set(redis_key, json.dumps(controller_state))
+    _setController(game_id, controller_id, controller_state)
 
 @app.route('/get_game_state/<game_id>')
+@game_exists
 def get_game_state(game_id):
-    game_state = json.loads(r.get(game_id))
-    num_controllers = int(game_state['num_controllers'])
+    game_state = _getGame(game_id)
+    num_controllers = game_state['num_controllers']
     
     for controller_id in xrange(0, num_controllers):
-        redis_key = '{0}/{1}'.format(game_id, controller_id)
         controller_name = 'controller{0}'.format(controller_id)
-        game_state[controller_name] = json.loads(r.get(redis_key))
+        game_state[controller_name] = _getController(game_id, controller_id)
 
     return json.dumps(game_state)
+
+@app.route('/get_ports/<game_id>')
+@game_exists
+def get_ports(game_id):
+    if not r.exists(game_id):
+        logging.error('Game {0} does not exist, could not connect.'.format(game_id))
+        return None
+    game_state = _getGame(game_id)
+    num_controllers = game_state['num_controllers']
+    available_ports = []
+    for controller_id in xrange(0, num_controllers):
+        controller_state = _getController(game_id, controller_id)
+        if controller_state['status'] == 'DISCONNECTED':
+            available_ports.append(controller_id)
+    return available_ports
--- write_socket.py
+++ write_socket.py
@@ -1,7 +1,7 @@
 from websocket import create_connection
 import time
 
-game_id = '5315a87d-b10a-45c3-94fd-8f0a42b120ef'
+game_id = '45c3-94fd-8f0a42b120ef'
 controller_id = 1
 ws = create_connection('ws://localhost:8000/connect_controller/{0}/{1}'.format(game_id, controller_id))
 while True:
