ecde89234e2e46f9afe1e0081fac497037cdffa4
pachuc <bpgoku2002@gmail.com>
2016-12-08 13:55:42 -0800

    clean up the main.js

--- sktz/static/javascripts/Main.js
+++ sktz/static/javascripts/Main.js
@@ -7,23 +7,19 @@ function Main(game_id, num_controllers){
     	var two = new Two({
     		width: w,
     		height: h
-    	});
-    	var game_state_string = 'ws://localhost:8000/get_game_state_persist/' + game_id;
-        var socket = new WebSocket(game_state_string);
-    
+    	});    
     	two.appendTo(elem);
     	var game = new Game(two, w, h, num_controllers);
-        socket.onmessage = function(event){
+   	
+	var game_state_string = 'ws://localhost:8000/get_game_state_persist/' + game_id;
+        var socket = new WebSocket(game_state_string);     
+	socket.onmessage = function(event){
         	game.updateControls(JSON.parse(event.data));
         };
+
 	two.bind('update', function(frameCount){
-		//yeah apparently i shouldn't be making synchronous ajax calls as I'm basically blocking the UI thread with a get
-		//and could possibly cause the browser to hang/crash
-		//var controller_data = JSON.parse($.ajax({type: "GET", url: game_state_string, async:false}).responseText);
-                //$.ajax({type: "GET", url: game_state_string, success: function(data){game.updateControls(JSON.parse(data));}, asyc:true});
                 game.update();
 	});
-
 	setInterval(function() {
   		two.update();
 	}, FRAMERATE);
