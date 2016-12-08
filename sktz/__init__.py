a3c7609ba3e5f13586435d4f3951d678db99d895
pachuc <bpgoku2002@gmail.com>
2016-12-08 14:02:35 -0800

    remove un-needed logging setup

--- sktz/__init__.py
+++ sktz/__init__.py
@@ -10,18 +10,12 @@ class Settings(Config):
         super(Settings, self).__init__(here)
         config = os.environ.get('SKTZ_CONFIG', DEFAULT_CONFIG)
         self.from_pyfile(config)
-        #self.setup_logging()
-    def setup_logging(self):
-        from logging.config import dictConfig
-        dictConfig(self.get('LOGGING', {'version':1}))
 
 settings = Settings()
 redis = from_url(settings['REDIS_URL'])
 app = Flask(__name__)
 app.config.update(settings)
 #stop flask app initialization from overriding our logger settings
-#app.logger
-#settings.setup_logging()
 sockets = Sockets(app)
 #register our views
 from sktz.views import html, ws
